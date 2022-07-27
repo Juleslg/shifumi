bb1743e3e6025837b4f148bfc6874884856acce3
Juleslg <julesradiolg@gmail.com>
2022-07-27 17:15:00 +0200

    added player and computer selections function, added playround function

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-const shifumi = ["Rock", "Paper", "Scissors"];
-const computerchoice = shifumi[Math.floor(Math.random()*shifumi.length)];
+
+const shifumi = ["rock", "paper", "scissors"];
+const computerchoice = shifumi[Math.floor(Math.random() * shifumi.length)];
 
 function getComputerChoice() {
-    return computerchoice;  
+    return computerchoice;
 }
+
+function getPlayerChoice() {
+    let promptplayer = prompt("Enter Rock, Paper Or Scissors").toLowerCase();
+
+
+    return promptplayer;
+
+}
+
+
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+function playRound(playerSelection, computerSelection) {
+
+    if ((playerSelection == "paper") && (computerSelection == "rock")) {
+
+        alert("Player wins");
+    }
+
+    else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+
+        alert("Player wins");
+    }
+
+    else if ((playerSelection == "paper") && (computerSelection == "paper")) {
+
+        alert("draw");
+    }
+
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
