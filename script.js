312aea3f5941ddc6bc2dad9dfdba02aa2e8728ab
Juleslg <julesradiolg@gmail.com>
2022-07-27 15:43:44 +0200

    added html + script files, added first function

---
+++ script.js
@@ -0,0 +1,6 @@
+const shifumi = ["Rock", "Paper", "Scissors"];
+const computerchoice = shifumi[Math.floor(Math.random()*shifumi.length)];
+
+function getComputerChoice() {
+    return computerchoice;  
+}
