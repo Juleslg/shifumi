1085d00afd31b2425d1ca60152d7e66237b171f8
Juleslg <julesradiolg@gmail.com>
2022-07-28 15:55:32 +0200

    added final game function and debugging playround function

--- script.js
+++ script.js
@@ -1,89 +1,116 @@
-let playerscore = 0
-let computerscore= 0
-
-
 
 
 const shifumi = ["rock", "paper", "scissors"];
-const computerchoice = shifumi[Math.floor(Math.random() * shifumi.length)];
+// const computerchoice = shifumi[Math.floor(Math.random() * shifumi.length)];
 
 function getComputerChoice() {
+    const computerchoice = shifumi[Math.floor(Math.random() * shifumi.length)];
     return computerchoice;
 }
 
 function getPlayerChoice() {
-    let promptplayer = prompt("Enter Rock, Paper Or Scissors").toLowerCase();
-
-
+    let promptplayer = prompt("Enter rock, paper or scissors").toLowerCase();
     return promptplayer;
-
+    
 }
 
-
-
-const computerSelection = getComputerChoice();
-const playerSelection = getPlayerChoice();
+let playerscore = 0
+let computerscore = 0
 
 function playRound(playerSelection, computerSelection) {
-
     if ((playerSelection == "paper") && (computerSelection == "rock")) {
 
-        alert("You win");
-        return playerscore += 1;
+        console.log("You win");
+        playerscore += 1;
     }
 
     else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
 
-        alert("You Loose");
-        return computerscore += 1;
-        
-    
+        console.log("You Loose");
+        computerscore += 1;
+
+
     }
 
     else if ((playerSelection == "paper") && (computerSelection == "paper")) {
 
-        alert("Replay");
-        return computerscore += 0;
+        console.log("Replay");
+        computerscore += 0;
     }
 
     if ((playerSelection == "rock") && (computerSelection == "rock")) {
 
-        alert("Replay");
-        return computerscore += 0;
+        console.log("Replay");
+        computerscore += 0;
     }
 
     else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
 
-        alert("You win");
-         return playerscore += 1;
+        console.log("You Win");
+        playerscore += 1;
     }
 
     else if ((playerSelection == "rock") && (computerSelection == "paper")) {
 
-        alert("You Loose");
-        return computerscore += 1;
+        console.log("You Loose");
+        computerscore += 1;
     }
 
     if ((playerSelection == "scissors") && (computerSelection == "rock")) {
 
-        alert("You Loose");
-        return computerscore += 1;
+        console.log("You Loose");
+        computerscore += 1;
     }
 
     else if ((playerSelection == "scissors") && (computerSelection == "scissors")) {
 
-        alert("Replay");
-        return computerscore += 0;
+        console.log("Replay");
+        computerscore += 0;
     }
 
     else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
 
+        console.log("You Win");
         alert("You Win");
-        return playerscore += 1;
+        playerscore += 1;
     }
 
+}
+
+function reset() {
+    playerscore = 0
+    computerscore = 0
     
 }
 
-console.log(playRound(playerSelection, computerSelection))
+
+function game() {
+
+    while (playerscore < 5 && computerscore < 5) {
+        playRound(getPlayerChoice(), getComputerChoice())
+
+    }
+
+    if (playerscore == 5) {
+        console.log("You Win The Game");
+        reset()
+    }
+
+    else if (computerscore === 5) {
+        console.log("You Loose You Loose The Game")
+        reset()
+
+    }
+
     
+
+}
+
+function main() {
+    while(true) {
+        game()
+    }
+}
+
+
+main()
