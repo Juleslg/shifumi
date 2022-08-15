bc9dcb1da95ce933e1a6d93b094c2325436f63d6
Juleslg <julesradiolg@gmail.com>
2022-08-15 16:09:22 +0200

    added addeventlistener system

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
+const rockButtons = document.querySelector('.rock')
+const paperButtons = document.querySelector('.paper')
+const scissorsButtons = document.querySelector('.scissors')
+const buttons = document.querySelector('.container')
+const results = document.querySelector('.results')
+const boardplayer = document.querySelector('.playerboard')
+const computerboard = document.querySelector('.computerboard')
 
 const shifumi = ["rock", "paper", "scissors"];
 // const computerchoice = shifumi[Math.floor(Math.random() * shifumi.length)];
@@ -8,84 +15,135 @@ function getComputerChoice() {
     return computerchoice;
 }
 
-function getPlayerChoice() {
-    let promptplayer = prompt("Enter rock, paper or scissors").toLowerCase();
-    return promptplayer;
+rockButtons.addEventListener("click", function () {
+    playerSelection = "rock"
+    playRound(playerSelection, getComputerChoice())
+    boardplayer.textContent = "Player Score :" + playerscore
+    computerboard.textContent = "Computer Score :" + computerscore 
+    if (playerscore == 5) {
+        results.textContent = "You Win The Game";
+        reset()
+    }
+    else if (computerscore === 5) {
+        results.textContent = "You Loose The Game"
+        reset()
+
+    }
     
-}
+});
+
+paperButtons.addEventListener("click", function () {
+    playerSelection = "paper"
+    playRound(playerSelection, getComputerChoice())
+    boardplayer.textContent = "Player Score :" + playerscore
+    computerboard.textContent = "Computer Score :" + computerscore 
+    if (playerscore == 5) {
+        results.textContent = "You Win The Game";
+        reset()
+    }
+    else if (computerscore === 5) {
+        results.textContent = "You Loose The Game"
+        reset()
+
+    }
+
+});
+
+scissorsButtons.addEventListener("click", function () {
+    playerSelection = "scissors"
+    playRound(playerSelection, getComputerChoice())
+    boardplayer.textContent = "Player Score :" + playerscore
+    computerboard.textContent = "Computer Score :" + computerscore 
+    if (playerscore == 5) {
+        results.textContent = "You Win The Game";
+        reset()
+    }
+    else if (computerscore === 5) {
+        results.textContent = "You Loose The Game"
+        reset()
+
+    }
+});
 
 let playerscore = 0
 let computerscore = 0
 
+
+
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection == "paper") && (computerSelection == "rock")) {
 
-        console.log("You win");
+        results.textContent = "You win";
         playerscore += 1;
     }
 
     else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
 
-        console.log("You Loose");
+        results.textContent = "You Loose";
         computerscore += 1;
 
     }
 
     else if ((playerSelection == "paper") && (computerSelection == "paper")) {
 
-        console.log("Replay");
+        results.textContent = "Replay";
         computerscore += 0;
     }
 
     if ((playerSelection == "rock") && (computerSelection == "rock")) {
 
-        console.log("Replay");
+        results.textContent = "Replay";
         computerscore += 0;
     }
 
     else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
 
-        console.log("You Win");
+        results.textContent = "You Win";
         playerscore += 1;
     }
 
     else if ((playerSelection == "rock") && (computerSelection == "paper")) {
 
-        console.log("You Loose");
+        results.textContent = "You Loose";
         computerscore += 1;
     }
 
     if ((playerSelection == "scissors") && (computerSelection == "rock")) {
 
-        console.log("You Loose");
+        results.textContent = "You Loose";
         computerscore += 1;
     }
 
     else if ((playerSelection == "scissors") && (computerSelection == "scissors")) {
 
-        console.log("Replay");
+        results.textContent = "Replay";
         computerscore += 0;
     }
 
     else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
 
-        console.log("You Win");
+        results.textContent = "You Win";
         playerscore += 1;
     }
+
+
+
+
+
 
 }
 
 function reset() {
     playerscore = 0
     computerscore = 0
-    
+
 }
 
 
-function game() {
+/* function game() {
 
     while (playerscore < 5 && computerscore < 5) {
-        playRound(getPlayerChoice(), getComputerChoice())
+        playRound(playerSelection, getComputerChoice())
     }
 
     if (playerscore == 5) {
@@ -110,4 +168,4 @@ function main() {
 }
 
 
-main()
+main() */
